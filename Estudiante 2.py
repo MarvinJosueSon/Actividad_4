1a6e268664250e9469589e7f6c92af4f5731783f
Marvin <sonmarvint2001@gmail.com>
2025-07-09 19:02:02 -0600

    Primer Actualizacion

---
+++ Estudiante 2.py
@@ -0,0 +1,33 @@
+class Medicamento:
+    def __init__(self,nombre,precio):
+        self.nombre = nombre
+        self.precio = precio
+    def Mostrar(self):
+        print(f"Nombre: {self.nombre}; Precio: {self.precio}")
+MedicamentosPila=[]
+
+while True:
+    try:
+        print("==Medicamentos==")
+        print("1. Ingresesar Medicamento")
+        print("2. Entregar Medicamento")
+        print("3. Salir")
+        opcion=input("Ingrese la opcion: ")
+        match opcion:
+            case "1":
+                try:
+                    nombreAux=input("Ingrese el nombre del medicamento: ")
+                    precioAux=int(input("Ingrese el precio del medicamento"))
+                    medicamentoAux=Medicamento(nombreAux,precioAux)
+                    MedicamentosPila.append(medicamentoAux)
+                except ValueError:
+                    print("ERROR EN LOS DATOS")
+            case "2":
+                if len(MedicamentosPila) > 0:
+                    print("Entregando el medicamento:")
+                    MedicamentosPila.pop().Mostrar()
+                else:
+                    print("No hay medicamentos por entregar.")
+
+    except ValueError:
+        print("ERROR EN LOS DATOS")
