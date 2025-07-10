19afbf26f1f293913e476ff492ea8135a8ab55b7
Marvin <sonmarvint2001@gmail.com>
2025-07-09 19:18:39 -0600

    Medicamentos

--- Estudiante 2.py
+++ Estudiante 2.py
@@ -9,13 +9,14 @@ while True:
         print("==Medicamentos==")
         print("1. Ingresesar Medicamento")
         print("2. Entregar Medicamento")
-        print("3. Salir")
+        print("3. Medicamentos pendientes")
+        print("4. Salir")
         opcion=input("Ingrese la opcion: ")
         match opcion:
             case "1":
                 try:
                     nombreAux=input("Ingrese el nombre del medicamento: ")
-                    precioAux=int(input("Ingrese el precio del medicamento"))
+                    precioAux=int(input("Ingrese el precio del medicamento: "))
                     medicamentoAux=Medicamento(nombreAux,precioAux)
                     MedicamentosPila.append(medicamentoAux)
                 except ValueError:
@@ -26,6 +27,14 @@ while True:
                     MedicamentosPila.pop().Mostrar()
                 else:
                     print("No hay medicamentos por entregar.")
-
+            case "3":
+                print("Medicamentos en cola: ")
+                for medicamento in MedicamentosPila:
+                    medicamento.Mostrar()
+            case "4":
+                print("Saliendo...")
+                break
+            case _:
+                print("Opcion no encontrada")
     except ValueError:
         print("ERROR EN LOS DATOS")
